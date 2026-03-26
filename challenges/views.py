5797e5131ffffedf4985f1be4b6bc7febf8ee092
Vinuthak <vinutha.kasaragod@gmail.com>
2026-03-26 11:00:57 +0100

    Make the url path dynamic

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat only healthy food!"
+    elif month == "february":
+        challenge_text = "Walk for 30 minutes!"
+    elif month == "march":
+        challenge_text = "learn 3 hours of django everyday!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(challenge_text)
